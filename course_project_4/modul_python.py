2fb3cefbedd2a15610e141f2fa2f0aebe79aeaa9
Semen Oskolkov <oskolkovsemen@yandex.ru>
2023-01-19 14:37:28 +0600

    Changed def get_category_by_id

--- course_project_4/modul_python.py
+++ course_project_4/modul_python.py
@@ -8,6 +8,7 @@ def get_product_by_id(config=None, id=None):  # Получение данных 
         "SELECT product_id, product_name, categories.category_name, unit_price FROM products JOIN categories USING(category_id)")
     product_catalog = [dict((cur.description[i][0], value)
               for i, value in enumerate(row)) for row in cur.fetchall()]
+
     cur.connection.close()
     return product_catalog[id-1]
 
@@ -25,8 +26,22 @@ def get_category_by_id(config=None, id=None):  # Получение данных
     cur = config.cursor()
     cur.execute(
         "SELECT category_id, category_name, description, products.product_name FROM categories JOIN products USING(category_id)")
-    category_catalog = [dict((cur.description[i][0], value)
+    category_product = [dict((cur.description[i][0], value)  # Получаем массив словарей с product_name
                             for i, value in enumerate(row)) for row in cur.fetchall()]
+    # print(category_product)
+
+    cur.execute(
+        "SELECT category_id, category_name, description FROM categories")
+    category_catalog = [dict((cur.description[i][0], value)  # # Получаем массив словарей без product_name
+                             for i, value in enumerate(row)) for row in cur.fetchall()]
+    # print(category_catalog)
+
+    for j in category_catalog:  # Дособираем category_catalog со списком товаров в категории
+        j['product_name'] = []
+        for i in category_product:
+            if i.get('category_name') == j.get('category_name'):
+                j['product_name'].append(i.get('product_name'))
+
     cur.connection.close()
     return category_catalog[id-1]
 
